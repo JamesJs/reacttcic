9e8c82757e335b0880176e2baa0db2e61a791d47
Thiago Baldino <thiagobaldino5@gmail.com>
2020-05-10 04:33:33 -0300

    Updates

---
+++ precache-manifest.8d002bed1ea51eaec4be7aa431e07b5a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c7631c99f13e231d882037a645770b9",
+    "url": "/teste/index.html"
+  },
+  {
+    "revision": "021edaa8e46f26236525",
+    "url": "/teste/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "84534d0378d0719c8fc7",
+    "url": "/teste/static/css/main.ba84db67.chunk.css"
+  },
+  {
+    "revision": "021edaa8e46f26236525",
+    "url": "/teste/static/js/2.30797c56.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/teste/static/js/2.30797c56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84534d0378d0719c8fc7",
+    "url": "/teste/static/js/main.42d58d10.chunk.js"
+  },
+  {
+    "revision": "0c0e6d77123656579691",
+    "url": "/teste/static/js/runtime-main.382b56d5.js"
+  },
+  {
+    "revision": "4499971f73cc9e8847dc6eaa7ae4f768",
+    "url": "/teste/static/media/UFSJ_logo_2018.4499971f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reacttcic/precache-manifest.92c8e5440e07469f38540412c0d47b67.js"
+  "/teste/precache-manifest.8d002bed1ea51eaec4be7aa431e07b5a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reacttcic/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/teste/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
