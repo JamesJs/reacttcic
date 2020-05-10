b7618ec0d1b43effed5dd6ebc5e7e60dadaaa98a
Thiago Baldino <thiagobaldino5@gmail.com>
2020-05-10 04:22:12 -0300

    Updates

---
+++ precache-manifest.92c8e5440e07469f38540412c0d47b67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1937839f8d303edf0f2e8a17c43cea2",
+    "url": "/reacttcic/index.html"
+  },
+  {
+    "revision": "021edaa8e46f26236525",
+    "url": "/reacttcic/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "f875743080085e51b904",
+    "url": "/reacttcic/static/css/main.ba84db67.chunk.css"
+  },
+  {
+    "revision": "021edaa8e46f26236525",
+    "url": "/reacttcic/static/js/2.30797c56.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reacttcic/static/js/2.30797c56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f875743080085e51b904",
+    "url": "/reacttcic/static/js/main.c0add0e8.chunk.js"
+  },
+  {
+    "revision": "e33dfd5a23e16680eda0",
+    "url": "/reacttcic/static/js/runtime-main.421706f8.js"
+  },
+  {
+    "revision": "4499971f73cc9e8847dc6eaa7ae4f768",
+    "url": "/reacttcic/static/media/UFSJ_logo_2018.4499971f.jpg"
+  }
+]);
